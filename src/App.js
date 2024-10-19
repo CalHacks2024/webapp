d7b66932546ef5413bf87e391fe74aab1ba90ba8
Alan Wang <alanwang093@gmail.com>
2024-10-19 13:26:09 -0700

    r3f

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import Router from './components/Router';
+import { ScrollControls, Scroll } from '@react-three/drei';
+import Spectacles from './components/Spectacles';
 
 function App() {
   return (
-    <div className="app">
-      <Router />
-    </div>
+    <>
+      <ScrollControls pages={1}>
+        <Spectacles />
+
+        <Scroll></Scroll>
+
+        <Scroll html style={{ width: '100%' }}>
+          <div className="app">
+            <Router />
+          </div>
+        </Scroll>
+      </ScrollControls>
+    </>
+    
+    // <div className="app">
+    //   <Router />
+    // </div>
   );
 }
 
